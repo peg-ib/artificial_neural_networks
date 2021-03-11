601631a105f2740c067efd7e4d0cd08da66c48ac
PEG <peg.iu8@gmail.com>
2021-03-11 16:08:11 +0300

    1 and 2 points completed

--- lab_01/single_layer_neural_networks.py
+++ lab_01/single_layer_neural_networks.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def boolean_function(x1, x2, x3, x4):
-    return (not x1 or not x2 or not x3) and (not x2 or not x3 or x4)
+    return int((not x1 or not x2 or not x3) and (not x2 or not x3 or x4))
 
 
 def value_table():
@@ -31,7 +31,7 @@ def softsign_activation_function(net):
 def evaluate_neural_network(input_data, weights, function_selection):
     net = 0
     for iteration in range(5):
-        net = sum(weights[iteration] * input_data[iteration])
+        net += weights[iteration] * input_data[iteration]
     if function_selection is True:
         return step_activation_function(net)
     else:
@@ -55,17 +55,31 @@ def correct_weight(weights, error, values, out, function_selection):
 
 def learning_neural_network(function_selection):
     weights = [0, 0, 0, 0, 0]
-    result = []
     bool_values = value_table()
     while True:
         errors = 0
+        result = []
+        res = []
         for value in bool_values:
             desired = boolean_function(value[1], value[2], value[3], value[4])
             actual, out = evaluate_neural_network(value, weights, function_selection)
+            res.append(desired)
             result.append(actual)
             error = evaluate_error(desired, actual)
             weights = correct_weight(weights, error, value, out, function_selection)
+            if actual != desired:
+                errors += 1
+        print(result)
+        print(errors)
+        if errors == 0:
+            break
 
+
+# True - step_activation_function
+# False - softsign_activation_function
 
 
 if __name__ == '__main__':
+    learning_neural_network(True)
+    learning_neural_network(False)
+
