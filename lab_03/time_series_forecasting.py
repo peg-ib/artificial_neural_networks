d68060606399141fb46a87d489ab0ba7650244f2
PEG <peg.iu8@gmail.com>
2021-04-15 00:16:11 +0300

    lab 03 complete.

--- lab_03/time_series_forecasting.py
+++ lab_03/time_series_forecasting.py
@@ -1,5 +1,6 @@
 import math
 import matplotlib.pyplot as plt
+from decimal import Decimal
 
 
 def function(x):
@@ -7,8 +8,10 @@ def function(x):
 
 
 def change_interval(left, right):
+    coordinate_x = coordinate(left, right, 20)
+    delta = coordinate_x[1] - coordinate_x[0]
     buf = left
-    left = right
+    left = right + delta
     right = 2 * right - buf
     return left, right
 
@@ -39,6 +42,8 @@ def calculation_desired_function(left, right):
 def error_calculation(desired, actual):
     error = 0
     for index in range(len(desired)):
+        desired[index] = Decimal(desired[index])
+        actual[index] = Decimal(actual[index])
         error += (desired[index] - actual[index]) ** 2
     error = math.sqrt(error)
     return error
@@ -67,13 +72,13 @@ def learning(number_epochs, ny, window_size, left, right):
     while era != number_epochs:
         window = desired_function[:window_size]
         for step in range(len(desired_function) - window_size):
-            new_window_element = evaluate_neural_network(window, weights)
-            delta = calculation_delta(desired_function[window_size + step], new_window_element)
+            new_element = evaluate_neural_network(window, weights)
+            delta = calculation_delta(desired_function[window_size + step], new_element)
             weights = correct_weights(weights, ny, delta, window)
-            window.append(new_window_element)
+            window.append(desired_function[window_size + step])
             window = window[1:]
         era += 1
-    return weights, desired_function
+    return weights
 
 
 def completion_function(weights, left, right, window_size):
@@ -88,7 +93,7 @@ def completion_function(weights, left, right, window_size):
         window.append(new_window_element)
         window = window[1:]
     error = error_calculation(desired_function, actual_function)
-    return error, actual_function, desired_function
+    return error, actual_function
 
 
 def plot(coordinate_x, desired_function, actual_function, line):
@@ -99,6 +104,66 @@ def plot(coordinate_x, desired_function, actual_function, line):
     ax.grid()
     ax.vlines(line, min(desired_function), max(desired_function), color='r')
     plt.show()
+
+
+def research(left, right, number_era, ny, window_size):
+    # error dependence on epochs
+    errors = []
+    number_epochs = []
+    for era in range(500, 10500, 500):
+        weights = learning(era, ny, window_size, left, right)
+        error, actual_function = completion_function(weights, left, right, window_size)
+        number_epochs.append(era)
+        errors.append(error)
+    plt.xlabel("M")
+    plt.ylabel("E")
+    plt.plot(number_epochs, errors)
+    plt.show()
+    # dependence of error on window size
+    errors = []
+    windows = []
+    for window in range(2, 20):
+        weights = learning(number_era, ny, window, left, right)
+        error, actual_function = completion_function(weights, left, right, window)
+        windows.append(window)
+        errors.append(error)
+    plt.xlabel("P")
+    plt.ylabel("E")
+    plt.plot(windows, errors)
+    plt.show()
+    # dependence of error on learning rate
+    errors = []
+    number_ny = []
+    for ny in range(10, 105, 5):
+        weights = learning(number_era, ny / 100, window_size, left, right)
+        error, actual_function = completion_function(weights, left, right, window_size)
+        number_ny.append(ny / 100)
+        errors.append(error)
+    plt.xlabel("ny")
+    plt.ylabel("E")
+    plt.plot(number_ny, errors)
+    plt.show()
+
+
+def print_result(left, right, number_era, ny, window_size):
+    buf = left
+    result_weights = learning(number_era, ny, window_size, left, right)
+    result_error, actual_function = completion_function(result_weights, left, right, window_size)
+    desired_function_left = calculation_desired_function(left, right)
+    left_coordinate = coordinate(left, right, 20)
+    left, right = change_interval(left, right)
+    desired_function_right = calculation_desired_function(left, right)
+    right_coordinate = coordinate(left, right, 20)
+    coordinate_x = left_coordinate + right_coordinate
+    desired_function = desired_function_left + desired_function_right
+    new_function = desired_function_left + actual_function
+    area = right - (right - buf) / 2
+    plot(coordinate_x, desired_function, new_function, area)
+    print('number of epochs = ', number_era)
+    print('window size = ', window_size)
+    print('ny = ', ny)
+    print("synaptic weights:", result_weights)
+    print("error = ", result_error)
 
 
 def brute_force(left, right):
@@ -112,7 +177,8 @@ def brute_force(left, right):
             for current_ny in range(10, 105, 5):
                 print("M =", current_number_era, "p =", current_window, "ny =", current_ny / 100)
                 current_weights = learning(current_number_era, current_ny / 100, current_window, left, right)
-                current_error = completion_function(current_weights, left, right, current_window)
+                print(current_weights)
+                current_error, actual_function = completion_function(current_weights, left, right, current_window)
                 print(current_error)
                 if current_error <= error:
                     number_era = current_number_era
@@ -127,65 +193,7 @@ def brute_force(left, right):
     print(weights)
 
 
-def research():
-    left = 0
-    right = 1
-    # error dependence on epochs
-    errors = []
-    number_era = []
-    for era in range(500, 10500, 500):
-        weights, desired_function = learning(era, 0.2, 2, left, right)
-        error, actual_function, desired_function = completion_function(weights, left, right, 2)
-        number_era.append(era)
-        errors.append(error)
-    plt.xlabel("M")
-    plt.ylabel("E")
-    plt.plot(number_era, errors)
-    plt.show()
-    # dependence of error on window size
-    errors = []
-    windows = []
-    for window in range(2, 20):
-        weights, desired_function = learning(1000, 0.2, window, left, right)
-        error, actual_function, desired_function = completion_function(weights, left, right, window)
-        windows.append(window)
-        errors.append(error)
-    plt.xlabel("P")
-    plt.ylabel("E")
-    plt.plot(windows, errors)
-    plt.show()
-    # dependence of error on learning rate
-    errors = []
-    number_ny = []
-    for ny in range(10, 105, 5):
-        weights, desired_function = learning(1000, ny/100, 2, left, right)
-        error, actual_function, desired_function = completion_function(weights, left, right, 2)
-        number_ny.append(ny/100)
-        errors.append(error)
-    plt.xlabel("ny")
-    plt.ylabel("E")
-    plt.plot(number_ny, errors)
-    plt.show()
-
-
-# M = 1000  p = 2  ny = 0.2  error = 0.09152081065116707
-
-
 if __name__ == '__main__':
-    # brute_force(0, 1)
-    left_x = 0
-    right_x = 1
-    area = 1
-    first_x_coordinate = coordinate(left_x, right_x, 20)
-    left_x, right_x = change_interval(0, 1)
-    second_x_coordinate = coordinate(left_x, right_x, 20)
-    result_weights, function_desired_left = learning(1000, 0.2, 2, 0, 1)
-    result_error, function_actual, function_desired_right = completion_function(result_weights, 0, 1, 2)
-    function_desired = function_desired_left + function_desired_right
-    plot(first_x_coordinate + second_x_coordinate, function_desired, function_desired_left + function_actual, area)
-    research()
-    print('number of epochs = 1000')
-    print('window size = 2')
-    print('ny = 0.2')
-    print("synaptic weights:", result_weights)
-    print("error = ", result_error)
+    brute_force(0, 1)
+    print_result(0, 1, 4000, 0.9, 4)
+    # research(0, 1, 4000, 0.9, 4)
